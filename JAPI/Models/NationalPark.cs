bee84ae3d3cc508aea03bd0f59fe964a1ea4fb4a
Nisanur Bulut <nisanur.bulut@siemens.com>
2021-11-22 21:35:43 +0200

    created NationalPark model

---
+++ JAPI/Models/NationalPark.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JAPI.Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        [Required]
+        public int State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
