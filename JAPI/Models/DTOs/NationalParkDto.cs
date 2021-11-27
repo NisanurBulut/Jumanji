9898f03c48b84ef35f3035b4b253d5150df28ca0
Nisanur Bulut <nisanur.bulut@siemens.com>
2021-11-27 13:57:44 +0200

    DataAnnotations for Dto

--- JAPI/Models/DTOs/NationalParkDto.cs
+++ JAPI/Models/DTOs/NationalParkDto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace JAPI.Models.Dtos
 {
     public class NationalParkDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int State { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
