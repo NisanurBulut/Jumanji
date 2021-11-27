8b817f6eeddd1b929c3aa5d2e6719f006f4a470d
Nisanur Bulut <nisanur.bulut@siemens.com>
2021-11-27 12:25:24 +0200

    implemented repository layer

---
+++ JAPI/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,17 @@
+﻿using JAPI.Models;
+using System.Collections.Generic;
+
+namespace JAPI.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark(int naionalParkId);
+        bool NationalParkExists(string name);
+        bool NationalParkExists(int id);
+        bool CreateNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool Save();
+    }
+}
---
+++ JAPI/Repository/NationalPArkRepository.cs
@@ -0,0 +1,49 @@
+﻿using JAPI.Models;
+using JAPI.Repository.IRepository;
+using System.Collections.Generic;
+
+namespace JAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public NationalPark GetNationalPark(int naionalParkId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
