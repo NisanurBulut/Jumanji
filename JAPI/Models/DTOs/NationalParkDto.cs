6346f5a477d1aff962aeae59c36d478f6eb11ce3
Nisanur Bulut <nisanur.bulut@siemens.com>
2021-11-27 12:54:07 +0200

    implemented DTO layer

---
+++ JAPI/Models/DTOs/NationalParkDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JAPI.Models.Dtos
+{
+    public class NationalParkDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
