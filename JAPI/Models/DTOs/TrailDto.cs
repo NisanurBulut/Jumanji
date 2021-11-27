e6e5a4132a9ffc6bd24af8cc15b75e2c966470fa
Nisanur Bulut <nisanur.bulut@siemens.com>
2021-11-27 18:12:00 +0200

    Trail model & DTO

---
+++ JAPI/Models/DTOs/TrailDto.cs
@@ -0,0 +1,22 @@
+﻿using JAPI.Models.Dtos;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static JAPI.Models.Trail;
+
+namespace JAPI.Models.DTOs
+{
+    public class TrailDto
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Distance { get; set; }
+        public DifficultyType Difficulty { get; set; }
+        [Required]
+        public int NationalParkId { get; set; }
+       
+        public NationalParkDto NationalPark { get; set; }
+    }
+}
